fc8e111c5743ec9f520ba1da2f435177ded47fdd
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-04-23 00:50:27 -0700

    fix: faster the node path update string
    This replaces regex with simple `startsWith` check that skips replacing of many string literals

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+/**
+ *
+ * @param babel {import('@babel/core')}
+*/
 module.exports = function removeNodePrefix(babel) {
   return {
     name: 'remove-node-prefix',
     visitor: {
+      /**
+       * @param path {import('@babel/traverse').NodePath<import('@babel/types').StringLiteral>}
+      */
       StringLiteral(path) {
         if (
-          path.parent.type === 'ImportDeclaration' ||
+          (path.parent.type === 'ImportDeclaration' ||
           (path.parent.type === 'CallExpression' &&
-            path.parent.callee.name === 'require')
+            path.parent.callee.name === 'require')) &&
+            path.node.value.startsWith('node:')
         ) {
+          // remove node: prefix from the path
           path.replaceWith(
-            babel.types.stringLiteral(path.node.value.replace(/^node:/, '')),
+            babel.types.stringLiteral(path.node.value.slice(5)),
           );
         }
         path.skip();

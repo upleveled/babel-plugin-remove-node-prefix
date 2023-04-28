f44f4eb281ec59aa6d0c8eabcd8decd6a75777bc
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-04-28 03:23:15 -0700

    fix: improve performance, allow further processing (#4)
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+/**
+ * @param babel {import('@babel/core')}
+ */
 module.exports = function removeNodePrefix(babel) {
   return {
     name: 'remove-node-prefix',
     visitor: {
+      /**
+       * @param path {import('@babel/traverse').NodePath<import('@babel/types').StringLiteral>}
+       */
       StringLiteral(path) {
-        if (
+        const isImportOrRequire =
           path.parent.type === 'ImportDeclaration' ||
           (path.parent.type === 'CallExpression' &&
-            path.parent.callee.name === 'require')
-        ) {
-          path.replaceWith(
-            babel.types.stringLiteral(path.node.value.replace(/^node:/, '')),
-          );
+            path.parent.callee.name === 'require');
+
+        if (isImportOrRequire && path.node.value.startsWith('node:')) {
+          // Remove `node:` prefix from the path
+          path.replaceWith(babel.types.stringLiteral(path.node.value.slice(5)));
         }
-        path.skip();
       },
     },
   };

63ebb11598822301cc4d62d3914e35c618008b8e
Karl Horky <karl.horky@gmail.com>
2023-04-28 12:21:26 +0200

    Format

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 /**
- *
  * @param babel {import('@babel/core')}
-*/
+ */
 module.exports = function removeNodePrefix(babel) {
   return {
     name: 'remove-node-prefix',
     visitor: {
       /**
        * @param path {import('@babel/traverse').NodePath<import('@babel/types').StringLiteral>}
-      */
+       */
       StringLiteral(path) {
-        if (
-          (path.parent.type === 'ImportDeclaration' ||
+        const isImportOrRequire =
+          path.parent.type === 'ImportDeclaration' ||
           (path.parent.type === 'CallExpression' &&
-            path.parent.callee.name === 'require')) &&
-            path.node.value.startsWith('node:')
-        ) {
-          // remove node: prefix from the path
-          path.replaceWith(
-            babel.types.stringLiteral(path.node.value.slice(5)),
-          );
+            path.parent.callee.name === 'require');
+
+        if (isImportOrRequire && path.node.value.startsWith('node:')) {
+          // Remove `node:` prefix from the path
+          path.replaceWith(babel.types.stringLiteral(path.node.value.slice(5)));
         }
       },
     },
